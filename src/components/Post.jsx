9b6ff5f0f9d0ca99814e25c6d8294f80b5c93ed1
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-24 11:40:07 -0300

    post.jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/brunobandeiraf.png" />
+          <div className={styles.authorInfo}>
+            <strong>Bruno Fernandes</strong>
+            <span>Education</span>
+          </div>
+        </div>
+
+        <time title="24 de Junho às 11:35h" dateTime="2023-06-23 11:35:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Conteúdo do Post que foi escrito sem sentido algum, apenas para ter alguma coisa aqui. 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #react #fogueteNaoTemRe</a></p>
+      </div>
+    </article>
+  )
+}
