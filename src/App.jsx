f771b61f0bef63b14d6ca05a8174cbd25fb24be8
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-24 10:44:25 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './Post'
 
+import styles from './App.module.css';
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author = "Bruno Fernandes"
-        content = "Bla la La bal ra real dae der"
-      />
-      <Post
-        author = "Bruno Fernandes"
-        content = "Bla la La bal ra real dae der"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Bruno Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Machado de Assis"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
