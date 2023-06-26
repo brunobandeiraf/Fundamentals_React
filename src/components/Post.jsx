af1890bf4605e7c5f32e93aa21b456dc15269c24
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-25 22:11:53 -0300

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,15 +7,35 @@ import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 export function Post({ author, publishedAt, content }) {
+  {/*
+    useState contendo o array de comentários
+  */}
   const [comments, setComments] = useState([
-    1,
-    2,
+    'Post muito bacana, hein?!'
   ]);
   
-  function handleCrateNewComment() {
-    event.preventDefault()
+  {/*
+    useState para capturar o valor do campo do comentário
+    useState('') - começa com um valor vazio
+  */}
+  const [newCommentText, setNewCommentText] = useState('');
 
-    setComments([...comments, comments.length + 1]);
+  function handleCreateNewComment() {
+    event.preventDefault()
+  {/*
+    ...comments -> pega TODOS os valores anteriores e 
+    setComments adicionar o novo valor newCommentText
+  */}
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  {/*
+    Funções é acionada quando o usuário digita no textarea
+    e pega o valor e insere no array, por meio do useState
+  */}
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
@@ -53,11 +73,21 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-       <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
+       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
+        {/*
+          value={newCommentText} - após capturar o valor, 
+          muda para o estado inicial - deixa sem texto o textarea
+          
+          onChange={handleNewCommentChange} - uma função que fica 
+          esperando alterações no textarea e chama a função
+        */}
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -68,7 +98,7 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.commentList}>
         {/* Percorre o array comments e cria o componente Comment*/}
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
