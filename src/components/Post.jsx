ff4b6f26e2c7c924fc0e2e12e2a62291b466e2fd
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-25 21:51:30 -0300

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,23 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+  
+  function handleCrateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+  }
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
@@ -14,6 +26,7 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true
   });
+
   return (
     <article className={styles.post}>
       <header>
@@ -40,7 +53,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+       <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -53,8 +66,10 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
+        {/* Percorre o array comments e cria o componente Comment*/}
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
