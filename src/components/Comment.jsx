fe41353c84bef319991b08fdd7549d4f14d80e3c
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-26 21:46:57 -0300

    like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,7 +14,10 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1);
+    //setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
